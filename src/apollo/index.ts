a3d425b6a9bf1a74c2dfb786d4bf0d6b5acf9a3f
신동석 <sssin00@gmail.com>
2021-08-05 15:07:16 +0900

    헤더 임시 인증 정보 추가

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -10,6 +10,9 @@ export const isLoggedInVar = makeVar(false);
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   uri: 'http://192.168.1.249:4000/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI4MTQzNDc2fQ.w4PxhW0KtTjty7LlfXVdQhQp_YKE-5o0J53MqFRVow8',
+  },
   defaultOptions: {watchQuery: {fetchPolicy: 'cache-and-network'}},
 });
 
